a1bf764b58c3ca0a202771e2c110a6f18dc477fc
lenmom <lenmom@126.com>
2018-08-28 17:27:31 +0800

    Add project files.

---
+++ threadSample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Thrift.Server;
+using Thrift.Transport;
+
+namespace threadSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TServerSocket serverTransport = new TServerSocket(7911, 0, false);
+            ThriftCase.Processor processor = new ThriftCase.Processor(new BusinessImpl());
+            TServer server = new TSimpleServer(processor, serverTransport);
+            Console.WriteLine("Starting server on port 7911 ...");
+            server.Serve();
+        }
+    }
+}
---
+++ thrift_client/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thrift.Protocol;
+using Thrift.Transport;
+
+namespace thrift_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TTransport transport = new TSocket("127.0.0.1", 7911);
+            TProtocol protocol = new TBinaryProtocol(transport);
+            ThriftCase.Client client = new ThriftCase.Client(protocol);
+
+            transport.Open();
+            try
+            {
+                int i = client.testCase1(1, 2, "3");
+                Console.WriteLine(i);
+
+                Dictionary<string, string> dic = new Dictionary<string, string>();
+                dic.Add("num1", "1");
+                dic.Add("num2", "2");
+                List<string> values = client.testCase2(dic);
+                foreach (var item in values)
+                {
+                    Console.WriteLine(item);
+                }
+
+
+            }
+            catch (Thrift.TApplicationException x)
+            {
+                Console.WriteLine(x.StackTrace);
+
+            }
+            finally
+            {
+                transport.Close();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
